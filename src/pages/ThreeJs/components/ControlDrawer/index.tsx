106ec81a65971337bdfc715bb5a9b994eca38cc5
qq_24380731 <qq_24380731@noreply.gitcode.com>
2024-12-05 17:35:18 +0800

    feat: 新增功能

---
+++ src/pages/ThreeJs/components/ControlDrawer/index.tsx
@@ -0,0 +1,26 @@
+import CameraControl from '@/pages/ThreeJs/components/apis/CameraControl';
+import { Drawer } from 'antd';
+import { forwardRef, Ref, useImperativeHandle, useRef, useState } from 'react';
+
+interface ControlDrawerProps {
+  ref?: any;
+  [name: string]: any;
+}
+const ConrolFrawer = (
+  props: ControlDrawerProps,
+  ref: Ref<unknown> | undefined,
+) => {
+  const drawerRef = useRef(null);
+
+  const [open, setOpen] = useState(false);
+  useImperativeHandle(ref, () => ({
+    setOpen,
+  }));
+  return (
+    <Drawer open={open}>
+      <CameraControl ref={drawerRef} />
+    </Drawer>
+  );
+};
+
+export default forwardRef(ConrolFrawer);
--- src/pages/ThreeJs/demo1-scene-cube/index.tsx
+++ src/pages/ThreeJs/demo1-scene-cube/index.tsx
@@ -1,8 +1,10 @@
-import { useEffect } from 'react';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import ControlDrawer from '@/pages/ThreeJs/components/ControlDrawer';
+import { Button } from 'antd';
+import { createRef, useEffect, useState } from 'react';
 import * as THREE from 'three';
 import styles from './index.less';
-
-const initThree = () => {
+const initThree = (carmera: any) => {
   //场景
   let scene = new THREE.Scene();
 
@@ -11,7 +13,8 @@ const initThree = () => {
   // aspect:可视区域横纵比。
   // near:眼睛能看到的最近垂直距离。
   // far：眼睛能看到的最远垂直距离
-  let camera = new THREE.PerspectiveCamera(75, 2, 0.5, 1000);
+  const { fov, aspect, near, far } = carmera;
+  let camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
   camera.position.z = 5;
 
   // 渲染器
@@ -42,12 +45,27 @@ const initThree = () => {
   animate();
 };
 export default function Page() {
+  const controlDrawerRef: any = createRef<any>();
+  const [carmeraParam, setCarmeraParam] = useState({
+    fov: 75,
+    aspect: 2,
+    near: 0.5,
+    far: 1000,
+  });
+
+  const openDrawer = () => {
+    if (controlDrawerRef.current) {
+      controlDrawerRef.current.setOpen(true);
+    }
+  };
   useEffect(() => {
-    initThree();
-  }, []);
+    initThree(carmeraParam);
+  }, [carmeraParam]);
   return (
     <div>
       <h1 className={styles.title}>ThreeJs Cube demo1</h1>
+      <Button onClick={openDrawer}>控制</Button>
+      <ControlDrawer ref={controlDrawerRef} setCarmeraParam={setCarmeraParam} />
       <div id="three-demo1-scene"></div>
     </div>
   );
